fd3271e67c0ef5730d56a3561169fec0b2ac97f5
leemms102 <leemms102@gmail.com>
2022-11-10 10:58:58 +0900

    added profile

--- src/main/java/com/example/awstest/config/auth/SecurityConfig.java
+++ src/main/java/com/example/awstest/config/auth/SecurityConfig.java
@@ -17,22 +17,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                .authorizeRequests()
-                // 권한대상 주소
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
-                // user만 /api/v1/** api 가능
-                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                .anyRequest().authenticated()
+                    .authorizeRequests()
+                    // 권한대상 주소
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    // user만 /api/v1/** api 가능
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
                 .and()
-                .logout()
-                // 로그아웃 성공 시 이동
-                .logoutSuccessUrl("/")
+                    .logout()
+                        // 로그아웃 성공 시 이동
+                        .logoutSuccessUrl("/")
                 .and()
-                // 진업점
-                .oauth2Login()
-                // OAuth2 로그인 성공 후 사용자 정보 가져올 떄 설정
-                .userInfoEndpoint()
-                // 소셜 로그인 성공 시 진행할 UserService 인터페이스 구현체 등록. 소셜 서비스에서 사용자 정보 가져온 상태에서 추가로 진행하도록 명시
-                .userService(customOAuth2UserService);
+                    // 진업점
+                    .oauth2Login()
+                        // OAuth2 로그인 성공 후 사용자 정보 가져올 떄 설정
+                        .userInfoEndpoint()
+                            // 소셜 로그인 성공 시 진행할 UserService 인터페이스 구현체 등록. 소셜 서비스에서 사용자 정보 가져온 상태에서 추가로 진행하도록 명시
+                            .userService(customOAuth2UserService);
     }
 }
